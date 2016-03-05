df3a5187462802447a05b9acc67e7214400a8838
Jeehae Park <jpark2973@gmail.com>
2016-03-04 21:02:16 -0800

    (feat/gulp) Add angular concat browserify

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var def = require('del');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
-var sourcemaps = require('gulp-sourcemaps');
+var maps = require('gulp-sourcemaps');
 var assign = require('lodash');
 
 var path = {
@@ -24,58 +24,10 @@ var path = {
  */
 var customOpts = {
   // entries: ['./node_modules/', './client/script', './client/templates'],
-  entries: ['./server/index.js'],
+  entries: ['./server/bundle.js'],
   debug: true
 };
 
-gulp.task('javascript', function () {
-  // set up the browserify instance on a task basis
-  var b = browserify({
-    entries: ['./server/index.js','./server/routes.js','./node_modules/'],
-    debug: true
-  });
-
-  return b.bundle()
-    .pipe(source('app.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-        // Add transformation tasks to the pipeline here.
-        .pipe(uglify())
-        .on('error', gutil.log)
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./client/dist/js/'));
-});
-
-// var opts = assign({}, watchify.args, customOpts);
-// var b = watchify(browserify(opts));
-
-
-// var bundle = function() {
-//   gutil.log('bundle.running');
-//   return b.bundle()
-//   // log errors if they happen
-//   .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-//   .pipe(source('bundle.js'))
-//   // optional, remove if you don't need to buffer file contents
-//   .pipe(buffer())
-//   // optional, remove if you don't want sourcemaps
-//   .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-//   // Add transformation tasks to the pipeline here
-//   .pipe(sourcemaps.write('./'))// writes .map file
-//   .pipe(gulp.dest('./dist'));
-// };
-
-// gulp.task('bundle', bundle);
-// b.on('update', bundle);
-// b.on('log', gutil.log);
-
-
-
-
-
-// gulp.task('clean', function () {
-//   return del(path.)
-// });
 
 gulp.task('lint', function() {
 	return gulp.src(path.scripts)
@@ -114,3 +66,11 @@ gulp.task('default',function() {
 	gutil.log('I have configured a gulpfile');
 });
 
+gulp.task('angular',function() {
+	return gulp.src( ['./node_modules/angular/angular.min.js',
+		'./node_modules/angular-ui-router/release/angular-ui-router.min.js'])
+    .pipe(maps.init())
+    .pipe(concat('angularApp.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('./client/dist/js/'));
+});
