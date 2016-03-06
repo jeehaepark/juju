e873c1a46292e29b14d4fa9b3c977357e8662bee
Madison <midunitz@ucdavis.edu>
2016-03-05 17:07:20 -0800

    update sql schema to work with postgres

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,58 +1,39 @@
-DROP DATABASE IF EXISTS juju;
 
-CREATE DATABASE juju;
-
-USE juju;
-
-
-DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  id INT NOT NULL AUTO_INCREMENT,
-  email VARCHAR(50) NOT NULL,
+  id INTEGER PRIMARY KEY, 
+  email VARCHAR(50) UNIQUE,
   phoneNumber VARCHAR(13),
-  password VARCHAR(250) NOT NULL,
-  userName VARCHAR(250) NOT NULL,
-  PRIMARY KEY (ID)
-);
+  password VARCHAR(250),
+  userName VARCHAR(250)
 
+)
 
-DROP TABLE IF EXISTS items;
 CREATE TABLE items (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
-  itemUrl VARCHAR(250) NOT NULL,
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(100),
+  itemUrl VARCHAR(250),
   itemImageUrl VARCHAR(250),
-  currentPrice int(9),
-  PRIMARY KEY (ID)
-);
+  currentPrice INTEGER
 
+)
 
-DROP TABLE IF EXISTS itemHistories;
 CREATE TABLE itemHistories (
-  id INT NOT NULL AUTO_INCREMENT,
-  price INT(9),
+  id INTEGER PRIMARY KEY,
+  price INTEGER,
   checkDate DATE,
-  itemID INT NOT NULL,
-  PRIMARY KEY (ID)
-);
-
-
-DROP TABLE IF EXISTS watchedItems;
+  itemID INTEGER,
+  FOREIGN KEY (itemID) REFERENCES items(id)
+)
 
 CREATE TABLE watchedItems (
-  id INT NOT NULL AUTO_INCREMENT,
+  id INTEGER PRIMARY KEY,
   deadline DATE,
-  idealPrice INT(9),
-  settlePrice int(9),
-  priceReached BOOL,
-  emailed BOOL,
-  itemID INT NOT NULL,
-  userID INT NOT NULL,
-  PRIMARY KEY (ID)
+  idealPrice INTEGER,
+  settlePrice INTEGER,
+  priceReached BOOLEAN,
+  emailed BOOLEAN,
+  FOREIGN KEY (itemID) REFERENCES items(id),
+  FOREIGN KEY (userID) REFERENCES users(id)
 );
 
-
-ALTER TABLE itemHistories ADD FOREIGN KEY (itemID) REFERENCES items (ID);
-ALTER TABLE watchedItems ADD FOREIGN KEY (itemID) REFERENCES items (ID);
-ALTER TABLE watchedItems ADD FOREIGN KEY (userID) REFERENCES users (ID);
 
