34877a05f7d6c0cb2b299c79c7ddd72bcb24d04e
Jeehae Park <jpark2973@gmail.com>
2016-03-07 19:39:37 -0800

    Add routs for table items

--- server/models/items.js
+++ server/models/items.js
@@ -1,24 +1,11 @@
-var express = require('express');
-var router = express.Router();
-var pg = require('pg');
-var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/juju';
-
-// var client = new pg.Client(connectionString);
-// client.connect();
-// var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-// query.on('end', function() { 
-  
-//   console.log('hello bitches')
-//   client.end(); });
-
 // CREATE A SINGLE USER
-//curl --data "email=juju@test.com2&phoneNumber=415-111-111&password=jujupw&userName=AdminJuJu" http://127.0.0.1:3000/api/v1/users
-router.post('/api/v1/users', function(req, res) {
+//curl --data 'email=juju@test.com2&phoneNumber=415-111-111&password=jujupw&userName=AdminJuJu' http://127.0.0.1:3000/api/v1/items
+router.post('/api/v1/items', function(req, res) {
 
     var results = [];
 
     // Grab data from http request
-    var data = {email: req.body.email, phoneNumber: req.body.phoneNumber, password:req.body.password, userName:req.body.userName};
+    var data = {name: req.body.name, itemUrl: req.body.itemUrl, itemImageUrl:req.body.itemImageUrl, currentPrice:req.body.currentPrice};
 
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
@@ -30,10 +17,10 @@ router.post('/api/v1/users', function(req, res) {
         }
 
         // SQL Query > Insert Data
-        client.query('INSERT INTO users(email, phoneNumber, password, userName) values($1, $2, $3, $4)', [data.email, data.phoneNumber, data.password, data.userName]);
+        client.query('INSERT INTO items(name, itemUrl, itemImageUrl, currentPrice) values($1, $2, $3, $4)', [data.name, data.itemUrl, data.itemImageUrl, data.currentPrice]);
 
         // SQL Query > Select Data
-        var query = client.query('SELECT * FROM users ORDER BY id ASC');
+        var query = client.query('SELECT * FROM items ORDER BY id ASC');
 
         // Stream results back one row at a time
         query.on('row', function(row) {
@@ -51,7 +38,7 @@ router.post('/api/v1/users', function(req, res) {
 });
 
 //READ GET ALL USERS
-router.get('/api/v1/users', function(req, res) {
+router.get('/api/v1/items', function(req, res) {
 
     var results = [];
 
@@ -65,7 +52,7 @@ router.get('/api/v1/users', function(req, res) {
         }
 
         // SQL Query > Select Data
-        var query = client.query('SELECT * FROM users ORDER BY id ASC;');
+        var query = client.query('SELECT * FROM items ORDER BY id ASC;');
 
         // Stream results back one row at a time
         query.on('row', function(row) {
@@ -83,16 +70,16 @@ router.get('/api/v1/users', function(req, res) {
 });
 
 //UPDATE A SINGLE USER
-//curl -X PUT --data "email=test@test.com&phoneNumber=510-111-1111&password=jujupw&userName=JuJu" http://127.0.0.1:3000/api/v1/users/1
-router.put('/api/v1/users/:user_id', function(req, res) {
+//curl -X PUT --data 'email=test@test.com&phoneNumber=510-111-1111&password=jujupw&userName=JuJu' http://127.0.0.1:3000/api/v1/users/1
+router.put('/api/v1/items/:item_id', function(req, res) {
 
     var results = [];
 
     // Grab data from the URL parameters
-    var id = req.params.user_id;
+    var id = req.params.item_id;
 
     // Grab data from http request
-    var data = {email: req.body.email, phoneNumber: req.body.phoneNumber, password:req.body.password, userName:req.body.userName};
+    var data = {name: req.body.name, itemUrl: req.body.itemUrl, itemImageUrl:req.body.itemImageUrl, currentPrice:req.body.currentPrice};
 
     // Get a Postgres client from the connection pool
     pg.connect(connectionString, function(err, client, done) {
@@ -104,10 +91,10 @@ router.put('/api/v1/users/:user_id', function(req, res) {
         }
 
         // SQL Query > Update Data
-        client.query('UPDATE users SET email=($1), phoneNumber=($2), password=($3), userName=($4) WHERE id=($5)', [data.email, data.phoneNumber, data.password, data.userName, id]);
+        client.query('UPDATE items SET email=($1), phoneNumber=($2), password=($3), userName=($4) WHERE id=($5)', [data.name, data.itemUrl, data.itemImageUrl, data.currentPrice, id]);
 
         // SQL Query > Select Data
-        var query = client.query('SELECT * FROM users ORDER BY id ASC');
+        var query = client.query('SELECT * FROM items ORDER BY id ASC');
 
         // Stream results back one row at a time
         query.on('row', function(row) {
@@ -122,6 +109,3 @@ router.put('/api/v1/users/:user_id', function(req, res) {
     });
 
 });
-
-
-module.exports = router;
