fd3b8eefa6ee6012964667449707547e651f9997
James Youn <jamesyoun710@gmail.com>
2016-03-08 21:53:50 -0800

    (DB models) Reformat indent spacing

--- server/models/database.js
+++ server/models/database.js
@@ -6,124 +6,120 @@ var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/ju
 // var client = new pg.Client(connectionString);
 // client.connect();
 // var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-// query.on('end', function() { 
-  
+// query.on('end', function() {
+
 //   console.log('hello bitches')
 //   client.end(); });
 
 // CREATE A SINGLE USER
 //curl --data "email=juju@test.com2&phoneNumber=415-111-111&FBuID=jujupw&userName=AdminJuJu" http://127.0.0.1:3000/api/v1/users
 router.post('/api/v1/users', function(req, res) {
-    var results = [];
+  var results = [];
 
-    // Grab data from http request
-    var data = {email: req.body.email, 
-                phoneNumber: req.body.phoneNumber, 
-                FBuID:req.body.FBuID, 
-                userName:req.body.userName};
+  // Grab data from http request
+  var data = {email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+    FBuID:req.body.FBuID,
+    userName:req.body.userName};
 
-    // Get a Postgres client from the connection pool
-    pg.connect(connectionString, function(err, client, done) {
-        // Handle connection errors
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).json({ success: false, data: err});
-        }
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({ success: false, data: err});
+    }
 
-        // SQL Query > Insert Data
+    // SQL Query > Insert Data
 
-        client.query('INSERT INTO users(email, phoneNumber, FBuID, userName) values($1, $2, $3, $4)', [data.email, data.phoneNumber, data.FBuID, data.userName]);
-//(routing)set up route to pass user info
+    client.query('INSERT INTO users(email, phoneNumber, FBuID, userName) values($1, $2, $3, $4)', [data.email, data.phoneNumber, data.FBuID, data.userName]);
+    //(routing)set up route to pass user info
 
-        // SQL Query > Select Data
-        var query = client.query('SELECT * FROM users ORDER BY id ASC');
+    // SQL Query > Select Data
+    var query = client.query('SELECT * FROM users ORDER BY id ASC');
 
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
-
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
-
-
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
     });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
 });
 
 //READ GET ALL USERS
 router.get('/api/v1/users', function(req, res) {
 
-    var results = [];
+  var results = [];
 
-    // Get a Postgres client from the connection pool
-    pg.connect(connectionString, function(err, client, done) {
-        // Handle connection errors
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).json({ success: false, data: err});
-        }
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({ success: false, data: err});
+    }
 
-        // SQL Query > Select Data
-        var query = client.query('SELECT * FROM users ORDER BY id ASC;');
+    // SQL Query > Select Data
+    var query = client.query('SELECT * FROM users ORDER BY id ASC;');
 
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
-
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
     });
+
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
 });
 
 //UPDATE A SINGLE USER
 //curl -X PUT --data "email=test@test.com&phoneNumber=510-111-1111&FBuID=jujupw&userName=JuJu" http://127.0.0.1:3000/api/v1/users/1
 router.put('/api/v1/users/:user_id', function(req, res) {
 
-    var results = [];
+  var results = [];
 
-    // Grab data from the URL parameters
-    var id = req.params.user_id;
+  // Grab data from the URL parameters
+  var id = req.params.user_id;
 
-    // Grab data from http request
-    var data = {email: req.body.email, phoneNumber: req.body.phoneNumber, FBuID:req.body.FBuID, userName:req.body.userName};
+  // Grab data from http request
+  var data = {email: req.body.email, phoneNumber: req.body.phoneNumber, FBuID:req.body.FBuID, userName:req.body.userName};
 
-    // Get a Postgres client from the connection pool
-    pg.connect(connectionString, function(err, client, done) {
-        // Handle connection errors
-        if(err) {
-          done();
-          console.log(err);
-          return res.status(500).send(json({ success: false, data: err}));
-        }
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, function(err, client, done) {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).send(json({ success: false, data: err}));
+    }
 
-        // SQL Query > Update Data
-        client.query('UPDATE users SET email=($1), phoneNumber=($2), FBuID=($3), userName=($4) WHERE id=($5)', [data.email, data.phoneNumber, data.FBuID, data.userName, id]);
+    // SQL Query > Update Data
+    client.query('UPDATE users SET email=($1), phoneNumber=($2), FBuID=($3), userName=($4) WHERE id=($5)', [data.email, data.phoneNumber, data.FBuID, data.userName, id]);
 
-        // SQL Query > Select Data
-        var query = client.query('SELECT * FROM users ORDER BY id ASC');
+    // SQL Query > Select Data
+    var query = client.query('SELECT * FROM users ORDER BY id ASC');
 
-        // Stream results back one row at a time
-        query.on('row', function(row) {
-            results.push(row);
-        });
-
-        // After all data is returned, close connection and return results
-        query.on('end', function() {
-            done();
-            return res.json(results);
-        });
+    // Stream results back one row at a time
+    query.on('row', function(row) {
+      results.push(row);
     });
 
+    // After all data is returned, close connection and return results
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
 });
-
 
 module.exports = router;
