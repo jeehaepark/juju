b28e68fd41ebdd1426283f2f0cc392b47c166924
Jeehae Park <jpark2973@gmail.com>
2016-03-08 16:37:42 -0800

    (feat/database) Get rid of * routes for now

--- server/routes.js
+++ server/routes.js
@@ -3,12 +3,15 @@ var app = require('./index.js')
 
 module.exports = function (app) {
 	app.get('/test', function (req, res) {
-		res.send('hello')
+		res.send('hello');
 	});
   app.post('/scrape',scrapeTools.scrape);
   //   res.send('hello')
   // });
-	app.get('*', function (req, res) {
-		res.redirct('/');
-	});
+ //  app.get('/api/v1/users', function (req, res){
+ //  	res.json(results);
+ //  })
+	// app.get('*', function (req, res) {
+	// 	res.redirct('/');
+	// });
 };
