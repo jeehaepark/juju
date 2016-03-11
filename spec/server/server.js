d80c6f0a1cafcc2152d769ce64db20f56158178e
James Youn <jamesyoun710@gmail.com>
2016-03-10 23:41:06 -0800

    WIP: save point - broken BE test

--- spec/server/server.js
+++ spec/server/server.js
@@ -1,5 +1,8 @@
 var request = require('request');
-var server = require('../../server/index.js');
+var app = require('../../server/index.js');
+var routes = require("../../server/routes.js");
+var express = require('express');
+var router = express.Router();
 
 var base_url = 'http://localhost:3000/';
 
@@ -13,7 +16,7 @@ describe('main page', function() {
       });
     });
 
-    it('should fine response.headers', function (done) {
+    it('should find response.headers', function (done) {
       request.get(base_url,function(error, response, body){
         expect(response.statusCode).toBe(200);
         expect(response.headers.connection).toBe('close');
@@ -34,6 +37,24 @@ describe('main page', function() {
       request.get('http://localhost:3000/api/users', function(error, response, body) {
         expect(body.success).not.toBe(false);
         expect(body).toBe('[]');
+        done();
+      });
+    });
+  });
+
+  describe('test routes', function(){
+    it('should be defined', function(done){
+      request.get('http://localhost:3000/test', function(error, response, body) {
+        expect(response.body).toBe('hello');
+        done();
+      });
+    });
+  });
+
+  describe('test router', function(){
+    it('should return users from db', function(done){
+      router.get('api/users', function(req, res) {
+        expect(req).toBe('something');
         done();
       });
     });
