ca6e198e323fe94497591d224afda87b6fc6247d
Jeehae Park <jpark2973@gmail.com>
2016-03-10 17:31:09 -0800

    (feat/database) Change itemUrl and imageUrl promptly

--- server/db/mockData.sql
+++ server/db/mockData.sql
@@ -2,7 +2,7 @@ USE juju;
 
 INSERT INTO users (email,phoneNumber,FBuID,userName) VALUES ('james@gmail.com', '949-466-4483', 'poop', 'hyoun44');
 
-INSERT INTO items (name,itemUrl,itemImageUrl,currentPrice) VALUES ('photo', 'www.photo.com', 'www.photo.com', '$7.88');
+INSERT INTO items (name,itemUrl,itemImageUrl,currentPrice) VALUES ('mug', 'http://www.amazon.com/The-Cottage-Industry-NA-Morning/dp/B005BR7JJM/ref=sr_1_2?s=kitchen&ie=UTF8&qid=1457658409&sr=1-2&keywords=mug', 'http://ecx.images-amazon.com/images/I/41FwckHUpJL._SX300_QL70_.jpg', '$4.88');
 
 INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$10.08', '2016-03-08', 1);
 INSERT INTO itemHistories (price,checkdate,itemID) VALUES ('$21.56', '2016-03-11', 1);
