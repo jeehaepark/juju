275fbbde8111d546ae78e8f4c782cef9faf61a94
James Youn <jamesyoun710@gmail.com>
2016-03-11 00:34:43 -0800

    (test) Add test for item controller

--- spec/client/item.spec.js
+++ spec/client/item.spec.js
@@ -1,9 +1,24 @@
 describe('Add item factory', function(){
+  var $scope;
+  var $rootScope;
   var Item;
+  var createController;
+  var $controller;
 
   beforeEach(module('juju'));
   beforeEach(inject(function ($injector) {
-    Item = $injector.get('Item');
+    $controller = $injector.get('$controller');
+    $rootScope = $injector.get('$rootScope');
+    $scope = $rootScope.$new();
+    Item = $injector.get('Item'); // this is okay
+
+    createController = function() {
+      return $controller('addItemCtrl', {
+        $scope: $scope,
+        Item: Item
+      });
+    };
+    createController();
   }));
 
   describe('Item Factory', function(){
@@ -19,6 +34,16 @@ describe('Add item factory', function(){
     // should imply the rest of the methods are accessible
     it('has scrapePriceInfo method', function(){
       expect(typeof Item.scrapePriceInfo).toBe('function');
+    });
+  });
+
+  describe('Item Controller', function(){
+    it('should have poop', function(){
+      expect($scope.test).toBe('poop');
+    });
+
+    it('should have add item function', function(){
+      expect(typeof $scope.addItem).toBe('function');
     });
   });
 });
