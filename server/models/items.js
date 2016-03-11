5532a61d343a510b2aadf6265634b0d1e30393de
Madison <midunitz@ucdavis.edu>
2016-03-10 18:49:57 -0800

    (integration) front end connects to database to add items and users

--- server/models/items.js
+++ server/models/items.js
@@ -33,57 +33,59 @@ router.post('/api/additems', function(req, res) {
     // SQL Query 
     var query = client.query({
       text : "SELECT id FROM items WHERE itemUrl = $1",
-          values : [data.itemUrl]}, function(err, result){
-            if(err){
-              console.log('err', err)
-            }else{
-              console.log('result', result)
-              if (result.rows.length===0){
-                console.log('item not yet in database')
-                client.query({
-                  text :'INSERT INTO items(name, itemUrl, itemImageUrl, currentPrice) values($1, $2, $3, $4) Returning id', 
-                  values : [data.name, data.itemUrl, data.itemImageUrl, data.currentPrice] }, function (err, result){
-                      if(err){
-                        console.log('err', err)
-                      }else{
-                        console.log('result of insert', result)
-                        data.itemId=result.rows[0].id;
-                        console.log('added item to the database, saving itemId', data)
-                      }
-                    })
-              }else{
-                data.itemId=result.rows[0].id;
-                data.itemExists=true;
-                console.log('data', data)
-              } 
-            }
+      values : [data.itemUrl]}, 
+      function(err, result){
+        if(err){
+          console.log('err', err);
+        }else{
+          console.log('result', result);
+          if (result.rows.length===0){
+            console.log('item not yet in database');
+            client.query({
+              text :'INSERT INTO items(name, itemUrl, itemImageUrl, currentPrice) values($1, $2, $3, $4) Returning id', 
+              values : [data.name, data.itemUrl, data.itemImageUrl, data.currentPrice] }, 
+              function (err, result){
+                if(err){
+                  console.log('err', err);
+                }else{
+                  console.log('result of insert', result);
+                  data.itemId=result.rows[0].id;
+                  console.log('added item to the database, saving itemId', data);
+                }
+              })
+          }else{
+              data.itemId=result.rows[0].id;
+              data.itemExists=true;
+              console.log('data', data);
+          } 
+        }
         if(!data.itemExists){
           client.query({
             text: 'INSERT INTO itemHistories(price, checkDate, itemID) values ($1, $2, $3)',
             values : [data.currentPrice, data.createdDate, data.itemId]
           }, function(err, result){
             if(err){
-              console.log(err)
+              console.log(err);
             }
           })
         }
         client.query({
           text: 'INSERT INTO watchedItems(idealPrice, priceReached, emailed, itemID, userID) values ($1, $2, $3, $4, $5)',
-          values: [data.idealPrice, false, false, data.itemId, data.userId]
-        }, function(err, result){
-          if (err){
-            console.log(err)
-          }else{
-            console.log('OMG I THINK WE DID IT!!!!')
-          }
-        })    
+          values: [data.idealPrice, false, false, data.itemId, data.userId]}, 
+          function(err, result){
+            if (err){
+              console.log(err);
+            }else{
+              console.log('OMG I THINK WE DID IT!!!!');
+            }
+          })    
         
         });
 
     // Stream results back one row at a time
     query.on('row', function(row) {
       results.push(row);
-      console.log('res in query on', res)
+      console.log('res in query on', res);
     });
 
     // After all data is returned, close connection and return results
