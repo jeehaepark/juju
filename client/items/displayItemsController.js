c5b4fee18dadfe92406325c5cea03c2a76a74486
Jeehae Park <jpark2973@gmail.com>
2016-03-14 16:39:42 -0700

    (refactor) Change double quotes to single quotes

--- client/items/displayItemsController.js
+++ client/items/displayItemsController.js
@@ -1,12 +1,12 @@
 angular.module('displayItemsController', [])
 .controller('displayItemsCtrl', function ($scope, displayItemsFactory, Auth){
-  console.log('Auth.userId', Auth.userId)
+  console.log('Auth.userId', Auth.userId);
   displayItemsFactory.getItemData(Auth.userId).then(
     function successCallback(response){
       console.log(response);
       $scope.itemData = response.data;
     }, function errorCallback(err){
       console.log(err);
-      $scope.err = "There was a problem loading your data";
-    })
-})
+      $scope.err = 'There was a problem loading your data';
+    });
+});
