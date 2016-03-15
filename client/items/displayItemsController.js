c1656188a59ec5e909bfec28cb7c7039e157c74c
Jeehae Park <jpark2973@gmail.com>
2016-03-14 20:51:20 -0700

    (feat/delete) Add delete items

--- client/items/displayItemsController.js
+++ client/items/displayItemsController.js
@@ -10,12 +10,12 @@ angular.module('displayItemsController', [])
       $scope.err = 'There was a problem loading your data';
     });
 
-  displayItemsFactoryFuncts.deleteData(item.id).then(function successCallback(response){
-      console.log(response);
-      $scope.itemData = response.data;
-    }, function errorCallback(err){
-      console.log(err);
-      $scope.err = 'There was a problem loading your data';
-    }
-  	);
+  // displayItemsFactoryFuncts.deleteData(item.id).then(function successCallback(response){
+  //     console.log(response);
+  //     $scope.itemData = response.data;
+  //   }, function errorCallback(err){
+  //     console.log(err);
+  //     $scope.err = 'There was a problem loading your data';
+  //   }
+  // 	);
 });
