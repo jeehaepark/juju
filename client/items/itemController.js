bfbfc4ba7f7681d74ed1143c21c1df33bc10818b
Jeehae Park <jpark2973@gmail.com>
2016-03-14 20:25:00 -0700

    (feat/delte) Delete item

--- client/items/itemController.js
+++ client/items/itemController.js
@@ -41,15 +41,15 @@ angular.module('juju.item', [])
       Item.addItemToDB($scope.item);
     }
   };
-  // $scope.displayItems = function() {
-  //   // console.log('Auth.userId', Auth.userId)
-  //   // displayItemsFactory.getItemData(Auth.userId.toString()).then(
-  //   //   function successCallback(response){
-  //   //     console.log(response);
-  //   //     $scope.itemData = response.data;
-  //   //   }, function errorCallback(err){
-  //   //     console.log(err);
-  //   //     $scope.err = "There was a problem loading your data";
-  //   //   })
-  // };
+  $scope.displayItems = function() {
+    console.log('Auth.userId', Auth.userId)
+    displayItemsFactory.getItemData(Auth.userId.toString()).then(
+      function successCallback(response){
+        console.log(response);
+        $scope.itemData = response.data;
+      }, function errorCallback(err){
+        console.log(err);
+        $scope.err = "There was a problem loading your data";
+      })
+  };
 });
--- client/users/userController.js
+++ client/users/userController.js
@@ -1,8 +1,14 @@
 angular.module('juju.user', [])
-.controller('usersCtrl', function($scope){
+.controller('usersCtrl', function($scope, userFactory){
   $scope.user={};
   $scope.user.email;
   $scope.user.phonenumber;
   $scope.user.username;
   // $scope.user.fbuid=Auth.userId;
+  User.getOneInfo().then(
+    function successCallback(res){
+      $scope.userData = res.data;
+    },function errorCallback(err){
+      $scope.err = 'fail to load your data';
+    });
 });
