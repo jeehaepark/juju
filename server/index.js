55aa9cd8b1a9d3a27bc47bb2723c81b74a7a1f49
James Youn <jamesyoun710@gmail.com>
2016-03-14 21:39:22 -0700

    (refactor) put scrape route in middleware

--- server/index.js
+++ server/index.js
@@ -4,11 +4,13 @@ var morgan = require('morgan');
 var cron = require('./cronJobs.js')
 var app = express();
 var router = express.Router();
+var scrapeTools = require('./scraping.js');
 
 require('./config/middleware.js')(app, express);
 
 var port = process.env.PORT || 3000;
 
+app.post('/scrape',scrapeTools.scrape);
 // app.config('test', function(){
 //     port = 3002;
 // });
