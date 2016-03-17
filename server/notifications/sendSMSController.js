b8d788891ac0a3362b8c8db7f70db5375e828177
Madison <midunitz@ucdavis.edu>
2016-03-17 10:46:02 -0700

    (notifications) add notification controller

---
+++ server/notifications/sendSMSController.js
@@ -0,0 +1,36 @@
+// Your accountSid and authToken from twilio.com/user/account
+//sudo npm install -g twilio
+//change stuff in the node modules twilio client file in order to make shit work.... probs not ideal...gotta figure out the env file thing
+
+var config = require('./../../config.js');
+var client = require('twilio')(config.accountSid, config.authToken);
+
+
+//var req={body: {}};
+
+module.exports = {
+  sendTextMessage : function (req, res){
+    //console.log('sendtextmessage req: ', req);
+    var itemNickname= req.body.nickname || 'oops';
+    var currentPrice = req.body.currentprice || 'oops';
+    var itemUrl = req.body.itemurl || 'oops';
+    var phoneNumber=req.body.phonenumber;
+    var message='Hello from Juju!\nThis is a notification that the item you were watching (' + itemNickname + ') is currently ' + currentPrice + ' which is equal to or less than the price you were interested in paying. \nYou can find the item at \n' +itemUrl +'\n Enjoy!' // The body of the text message
+
+    //var client = require('twilio')('AC420b878efbf51bb91fb5260ecef6b3b1', '759b77f8c566b9efdf37f7cd786d3bdc');
+    client.sendMessage({
+        to: phoneNumber, // the number for the phone in your pocket
+        from:'+16513832409', // your Twilio number
+        body: message // The body of the text message
+    }, function(error, message) {
+        // This callback is executed when the request completes
+        if (error) {
+            console.error('Sorry.  We couldn\'t send the message',error);
+        } else {
+            console.log('Message sent! Message id: '+message.sid);
+            res.send(message);
+        }
+    });
+  }
+
+};
