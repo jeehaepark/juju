1b04dd90d04d0f8f482d93b778b4d1150bc014ad
Madison <midunitz@ucdavis.edu>
2016-03-17 11:59:41 -0700

    (cron) add scrape to cron job

--- server/cronJobs.js
+++ server/cronJobs.js
@@ -1,39 +1,71 @@
 var CronJob = require('cron').CronJob;
 var Promise = require("bluebird");
 var request = Promise.promisifyAll(require("request"));
+var requestMultiArg = Promise.promisifyAll(require("request"), {multiArgs: true});
 var scrapeTool = require('./scraping.js');
 
 module.exports = {
   itemHistory : function (){
     var allItems;
-    new CronJob('00-60 * * * * *' , function () {
+    new CronJob('01 01-60 * * * *' , function () {
+      //console.log('ppooop');
       request.getAsync('http://localhost:3000/api/items')
-      .then(function(v){
-        var items = JSON.parse(v.body);
-        var itemUrlArr = [];
+      .then(function(res){
+        //console.log('res', res.body);
 
-        for(var i = 0; i < items.length; i++){
-          itemUrlArr.push(items[i].itemurl);
-        };
+        var items = JSON.parse(res.body);
+        return items;
+      })
+      .then(function(items){
 
-        return itemUrlArr;
+        //create an array to push the reulst objs to
+        var resultArray = [];
+        Promise.each(items, function(item){
+          // console.log('item: ', item);
+          //do scrape request in here
+          var options = {
+            url: 'http://localhost:3000/scrape',
+            method: 'POST',
+            form: {'url': item.itemurl}
+          }
+
+          return requestMultiArg.postAsync(options).then(function(response, body){
+            console.log('inside each - response.body', response.body)
+
+            if(response.statusCode===200){
+              var price=response.body.price;
+            }
+            //push obj containing currentprice and item id to resultArray
+            var pushResults = {id: item.id, currentPrice: price}
+            // console.log('push results: ', pushResults);
+            resultArray.push(pushResults)
+          })
+        })
+        // console.log(resultArray.length)
+        // return resultArray;
+      })
+
+
+
+      .then(function(items){
+        // console.log('made it to here', items);
       })
       .catch(function(e){
         console.log('error', e);
       })
-      .then(function(itemUrlArr){
+      // .then(function(itemUrlArr){
 
-        // building up req/res parameters to inject in scrape function
-        for(var i = 0; i < itemUrlArr.length; i++){
-          var reqBodyUrl = {
-            body: {
-              url : itemUrlArr[i]
-            }
-          }
-          scrapeTool.scrape(reqBodyUrl);
-        }
-        // console.log(scrapeTool.scrape);
-      });
+      //   // building up req/res parameters to inject in scrape function
+      //   for(var i = 0; i < itemUrlArr.length; i++){
+      //     var reqBodyUrl = {
+      //       body: {
+      //         url : itemUrlArr[i]
+      //       }
+      //     }
+      //     scrapeTool.scrape(reqBodyUrl);
+      //   }
+      // console.log(scrapeTool.scrape);
+      //});
       // // check getAsync's callbacks or use callbacks to handle the async results
       // .then(function(err, res, body) {
       //   // console.log(JSON.parse(body));
