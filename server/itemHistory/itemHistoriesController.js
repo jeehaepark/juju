90567133721f88e6c9b27313f51f48b4108bd1d5
James Youn <jamesyoun710@gmail.com>
2016-03-17 22:40:29 -0700

    (refactor) removing unused dependencies

--- server/itemHistory/itemHistoriesController.js
+++ server/itemHistory/itemHistoriesController.js
@@ -1,5 +1,3 @@
-var express = require('express');
-var router = express.Router();
 var pg = require('pg');
 var connectionString = process.env.DATABASE_URL || require('../../config.js').connectionString;
 
