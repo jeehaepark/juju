aaf66f46fc582c42eb9c7f8b8aaf0848a304bfba
Madison <midunitz@ucdavis.edu>
2016-03-18 11:10:55 -0700

    (db) Add mock data

--- server/db/preloadedSchemas/mockData.sql
+++ server/db/preloadedSchemas/mockData.sql
@@ -4,8 +4,17 @@
   #########
 */
 
-INSERT INTO users (email,phoneNumber, contactPref, FBuID,FBname) VALUES ('james@gmail.com', '949-466-4483', 'email', 'poop', 'hyoun44');
-INSERT INTO users (email,phoneNumber, contactPref, FBuID,FBname) VALUES ('joe@gmail.com', '555-555-5555', 'text', 'peep', 'chiral');
+
+INSERT INTO users (email, phoneNumber, contactPref, FBuID, FBname) Values ('dunitzm@gmail.com',       '952-454-7366',  'email',   '123456',  'madisonEmail');
+INSERT INTO users (email, phoneNumber, contactPref, FBuID, FBname) Values ('jpark2973@gmail.com',     '415-602-9974',  'email',   '234567',  'jessicaEmail');
+INSERT INTO users (email, phoneNumber, contactPref, FBuID, FBname) Values ('jashby473@gmail.com',     '510-220-5077',  'email',   '345678',  'joeEmail');
+INSERT INTO users (email, phoneNumber, contactPref, FBuID, FBname) Values ('jamesyoun710@gmail.com',  '949-466-4483',  'email',   '456789',  'jamesEmail');
+INSERT INTO users (email, phoneNumber, contactPref, FBuID, FBname) Values ('madison@gmail.com',       '952-454-7366',  'text' ,   '567890',  'madisonText');
+INSERT INTO users (email, phoneNumber, contactPref, FBuID, FBname) Values ('jessica@gmail.com',       '415-602-9974',  'text' ,   '678901',  'jessicaText');
+INSERT INTO users (email, phoneNumber, contactPref, FBuID, FBname) Values ('joe@gmail.com',           '510-220-5077',  'text' ,   '789012',  'joeText');
+INSERT INTO users (email, phoneNumber, contactPref, FBuID, FBname) Values ('james@gmail.com',         '949-466-4483',  'text' ,   '890123',  'jamesText');
+INSERT INTO users (email, phoneNumber, contactPref, FBuID, FBname) Values ('madison2@gmail.com',      '952-454-7366',  'text' ,   '901234',  'madisonText2');
+INSERT INTO users (email, phoneNumber, contactPref, FBuID, FBname) Values ('madison3@gmail.com',      '952-454-7366',  'text' ,   '012345',  'madisonText3');
 
 
 /*
@@ -13,21 +22,17 @@ INSERT INTO users (email,phoneNumber, contactPref, FBuID,FBname) VALUES ('joe@gm
   # ITEMS #
   #########
 */
-INSERT INTO items (productTitle,itemUrl,itemImageUrl,currentPrice)
-VALUES (
-  'light',
-  'http://www.amazon.com/gp/product/B00LXTP2FA/ref=s9_simh_gw_g468_i1_r?ie=UTF8&fpl=fresh&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-1&pf_rd_r=1AP11P4BPYNJ3KJ17WJ4&pf_rd_t=36701&pf_rd_p=2079475242&pf_rd_i=desktop',
-  'http://ecx.images-amazon.com/images/I/71mKsMqkvvL._SX425_.jpg',
-  '$5.00'
-);
 
-INSERT INTO items (productTitle,itemUrl,itemImageUrl,currentPrice)
-VALUES (
-  'light',
-  'http://www.amazon.com/Torero-Inflatables-Dancer-Complete-20-Feet/dp/B00ASSF6MQ/ref=sr_1_1?s=sporting-goods&ie=UTF8&qid=1457732654&sr=1-1&keywords=air+dancer',
-  'http://ecx.images-amazon.com/images/I/81QatMHsj-L._SY355_.jpg',
-  '$200.00'
-);
+INSERT INTO items (productTitle, itemUrl, itemImageUrl, currentPrice) VALUES ('Cygolite Expilion 720 USB Light',                 'http://www.amazon.com/gp/product/B00LXTP2FA/ref=s9_simh_gw_g468_i1_r?ie=UTF8&fpl=fresh&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-1&pf_rd_r=1AP11P4BPYNJ3KJ17WJ4&pf_rd_t=36701&pf_rd_p=2079475242&pf_rd_i=desktop', 'http://ecx.images-amazon.com/images/I/71mKsMqkvvL._SX425_.jpg',        '$79.99');
+INSERT INTO items (productTitle, itemUrl, itemImageUrl, currentPrice) VALUES ('Torero Inflatables Air Dancer Tube Man Fly Guy',  'http://www.amazon.com/Torero-Inflatables-Dancer-Complete-20-Feet/dp/B00ASSF6MQ/ref=sr_1_1?s=sporting-goods&ie=UTF8&qid=1457732654&sr=1-1&keywords=air+dancer',                                                 'http://ecx.images-amazon.com/images/I/81QatMHsj-L._SY355_.jpg',       '$278.00');
+INSERT INTO items (productTitle, itemUrl, itemImageUrl, currentPrice) VALUES ('Assorted-Candy-Party-Mix-Bulk',                   'http://www.amazon.com/Assorted-Candy-Party-Mix-Bulk/dp/B017USCVMY/ref=sr_1_1_s_it?s=grocery&ie=UTF8&qid=1458095912&sr=1-1&keywords=candy',                                                                     'http://ecx.images-amazon.com/images/I/61mxZnj23fL.jpg',                '$22.99');
+INSERT INTO items (productTitle, itemUrl, itemImageUrl, currentPrice) VALUES ('100 Foot Multicolor Pennant Banner',              'http://www.amazon.com/100-Foot-Multicolor-Pennant-Banner/dp/B00519PJTW/ref=pd_sim_468_2?ie=UTF8&dpID=41bp-GHC-TL&dpSrc=sims&preST=_AC_UL160_SR160%2C160_&refRID=0YQJHJA8Z3FK9V342ZNH',                         'http://ecx.images-amazon.com/images/I/81BCndDEtxL._SX522_.jpg',         '$8.50');
+INSERT INTO items (productTitle, itemUrl, itemImageUrl, currentPrice) VALUES ('Paper Carnival Photo Booth Stick Props',          'http://www.amazon.com/Paper-Carnival-Photo-Booth-Stick/dp/B00R8FRC92/ref=pd_sim_21_1?ie=UTF8&dpID=51EwLHGhS7L&dpSrc=sims&preST=_AC_UL160_SR160%2C160_&refRID=0ACJ742YKQA2FRB1YVMW',                            'http://ecx.images-amazon.com/images/I/71I-8YsrJjL._SL1500_.jpg',       '$10.04');
+INSERT INTO items (productTitle, itemUrl, itemImageUrl, currentPrice) VALUES ('Carnival Scene Kit',                              'http://www.amazon.com/dp/B00ULPCDUE?psc=1',                                                                                                                                                                    'http://ecx.images-amazon.com/images/I/81OQa7FXDaL._SL1500_.jpg',      '$159.99');
+INSERT INTO items (productTitle, itemUrl, itemImageUrl, currentPrice) VALUES ('Big Top Canopy Tent Party Accessory',             'http://www.amazon.com/Big-Canopy-Tent-Party-Accessory/dp/B006WY4CN4/ref=pd_sim_21_6?ie=UTF8&dpID=61k2HdRBhIL&dpSrc=sims&preST=_AC_UL160_SR160%2C160_&refRID=1A6YXTXET2A5Z31SS414',                             'http://ecx.images-amazon.com/images/I/81BaaArmJlL._SL1500_.jpg',       '$42.47');
+INSERT INTO items (productTitle, itemUrl, itemImageUrl, currentPrice) VALUES ('kindle',                                          'http://www.amazon.com/dp/B00I15SB16/ref=ods_gw_d_h1_eink_bn?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-hero-kindle-A&pf_rd_r=0P45XSR90ARZ24E8SDWJ&pf_rd_t=36701&pf_rd_p=2433108262&pf_rd_i=desktop',                'http://ecx.images-amazon.com/images/I/61W0XGb88nL._SL1000_.jpg',       '$79.99');
+INSERT INTO items (productTitle, itemUrl, itemImageUrl, currentPrice) VALUES ('Jack Black Double-Duty Face Moisturizer SPF 20',  'http://www.amazon.com/Jack-Black-Double-Duty-Face-Moisturizer/dp/B000HP9M3E/ref=sr_1_2_s_it?s=luxury-beauty&ie=UTF8&qid=1458097114&sr=1-2',                                                                    'http://ecx.images-amazon.com/images/I/41fCKlGVn0L.jpg',                '$27.50');
+INSERT INTO items (productTitle, itemUrl, itemImageUrl, currentPrice) VALUES ('stila One Step Correct',                          'http://www.amazon.com/stila-One-Step-Correct-fl/dp/B003W5GL5I/ref=sr_1_5_s_it?s=luxury-beauty&ie=UTF8&qid=1458097114&sr=1-5',                                                                                  'http://ecx.images-amazon.com/images/I/71Ay6slM1dL._SY679_.jpg',        '$34.00');
 
 
 /*
@@ -36,15 +41,57 @@ VALUES (
   ##################
 */
 
-/* Item #1 */
-INSERT INTO itemHistories (price,checkDate,itemID) VALUES ('$10.08', '2016-03-08', 1);
-INSERT INTO itemHistories (price,checkdate,itemID) VALUES ('$21.56', '2016-03-11', 1);
-INSERT INTO itemHistories (price,checkdate,itemID) VALUES ('$53.11', '2016-03-10', 1);
 
-/* Item #2 */
-INSERT INTO itemHistories (price,checkdate,itemID) VALUES ('$300.11', '2016-03-10', 2);
-INSERT INTO itemHistories (price,checkdate,itemID) VALUES ('$250.11', '2016-03-10', 2);
-
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$79.99',  '2016-03-08',  1);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$85.00',  '2016-03-09',  1);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$75.00',  '2016-03-10',  1);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$78.00',  '2016-03-11',  1);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$79.99',  '2016-03-12',  1);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$278.00',  '2016-03-08',  2);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$200.00',  '2016-03-09',  2);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$290.00',  '2016-03-10',  2);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$285.00',  '2016-03-11',  2);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$278.00',  '2016-03-12',  2);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$22.99',  '2016-03-08',  3);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$25.00',  '2016-03-09',  3);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$20.00',  '2016-03-10',  3);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$21.00',  '2016-03-11',  3);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$22.99',  '2016-03-12',  3);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$8.50',  '2016-03-08',  4);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$10.00',  '2016-03-09',  4);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$8.00',  '2016-03-10',  4);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$9.00',  '2016-03-11',  4);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$8.50',  '2016-03-12',  4);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$10.04',  '2016-03-08',  5);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$15.00',  '2016-03-09',  5);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$11.23',  '2016-03-10',  5);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$9.89',  '2016-03-11',  5);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$10.04',  '2016-03-12',  5);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$159.99',  '2016-03-08',  6);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$150.00',  '2016-03-09',  6);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$140.00',  '2016-03-10',  6);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$166.00',  '2016-03-11',  6);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$159.99',  '2016-03-12',  6);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$42.47',  '2016-03-08',  7);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$40.00',  '2016-03-09',  7);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$45.00',  '2016-03-10',  7);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$34.00',  '2016-03-11',  7);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$42.47',  '2016-03-12',  7);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$79.99',  '2016-03-08',  8);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$65.00',  '2016-03-09',  8);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$70.00',  '2016-03-10',  8);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$72.01',  '2016-03-11',  8);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$79.99',  '2016-03-12',  8);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$27.50',  '2016-03-08',  9);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$22.30',  '2016-03-09',  9);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$24.50',  '2016-03-10',  9);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$21.12',  '2016-03-11',  9);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$27.50',  '2016-03-12',  9);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$34.00',  '2016-03-08', 10);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$33.00',  '2016-03-09', 10);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$30.00',  '2016-03-10', 10);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$36.00',  '2016-03-11', 10);
+INSERT INTO itemHistories (price, checkDate, itemID) VALUES ('$34.00',  '2016-03-12', 10);
 
 /*
   #################
@@ -52,14 +99,36 @@ INSERT INTO itemHistories (price,checkdate,itemID) VALUES ('$250.11', '2016-03-1
   #################
 */
 
-/* User #1 */
-INSERT INTO watchedItems (deadline, nickName, idealPrice,settlePrice,priceReached,emailed,itemID,userID)
-VALUES ('2016-04-01', 'shoes (with poop for james)', '$15.00', '$19.98', false, false, 1, 1);
 
-INSERT INTO watchedItems (deadline, nickName, idealPrice,settlePrice,priceReached,emailed,itemID,userID)
-VALUES ('2016-04-01', 'poopShirt', '$12.99', '$17.22', true, false, 2, 1);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'Dancing Guy',          '$250.00', '', 'f',   'f',    2,     1);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'yummy candy',           '$15.00', '', 'f',   'f',    3,     3);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'flagss',                 '$8.00', '', 'f',   'f',    4,     3);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'flag banners',           '$6.00', '', 'f',   'f',    4,     4);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'photo props',           '$10.00', '', 'f',   'f',    5,     4);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'carnival stuff',       '$159.00', '', 'f',   'f',    6,     4);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'carnival kit',         '$140.00', '', 'f',   'f',    6,     5);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'canopy tent',           '$15.00', '', 'f',   'f',    7,     5);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'big top canopy tent',   '$40.00', '', 'f',   'f',    7,     7);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'lotion',                '$15.00', '', 'f',   'f',    9,     7);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'baby kindle',           '$65.00', '', 'f',   'f',    8,     8);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'stila',                 '$15.00', '', 'f',   'f',   10,     8);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'jack black lotion',     '$27.00', '', 'f',   'f',    9,     9);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'bike light',            '$60.00', '', 'f',   'f',    1,     9);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'inflatable dude',      '$200.00', '', 'f',   'f',    2,    10);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'carnival kit',         '$150.00', '', 'f',   'f',    6,    10);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'light',                 '$85.00', '', 't',   'f',    1,     1);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'candy',                 '$22.99', '', 't',   'f',    3,     1);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'Dee Dancer',           '$280.00', '', 't',   'f',    2,     2);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'assorted candy',        '$27.00', '', 't',   'f',    3,     2);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'banner',                '$15.00', '', 't',   'f',    4,     2);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'props',                 '$15.00', '', 't',   'f',    5,     3);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'photo booth toys',      '$10.04', '', 't',   'f',    5,     5);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'carnival kits',        '$175.00', '', 't',   'f',    6,     6);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'big top!',              '$50.00', '', 't',   'f',    7,     6);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'kindle',                '$85.00', '', 't',   'f',    8,     6);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'kindle',                '$80.00', '', 't',   'f',    8,     7);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'manly lotion',          '$35.00', '', 't',   'f',    9,     8);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'lotion',                '$40.00', '', 't',   'f',   10,     9);
+INSERT INTO watchedItems (deadline, nickName, idealPrice, settlePrice, priceReached, emailed, itemID, userID) VALUES ('2016-04-01', 'flags',                 '$15.00', '', 't',   't',    4,    10);
 
-/* User #2 */
-INSERT INTO watchedItems (deadline, nickName, idealPrice,settlePrice,priceReached,emailed,itemID,userID)
-VALUES ('2016-04-01', 'candyBar', '$12.99', '$17.22', true, true, 2, 2);
 
