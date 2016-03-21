2f49d16d9452c458ac77a22c010f4db7ef8103d9
James Youn <jamesyoun710@gmail.com>
2016-03-21 14:03:18 -0700

    (refactor) change funciton name

--- server/watchedItems/watchedItemRoutes.js
+++ server/watchedItems/watchedItemRoutes.js
@@ -5,5 +5,5 @@ module.exports = function (router) {
   router.get('/',watchedItemController.getAllWatchedItems);
   router.put('/:watchedItem_id',watchedItemController.updateWatchedItem);
   router.delete('/watchedItem_id',watchedItemController.deleteWatchedItem);
-  router.get('/user/:user_id', watchedItemController.joinTable);
+  router.get('/user/:user_id', watchedItemController.getUserWatchedItems);
 };
