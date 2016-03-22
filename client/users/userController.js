677f45bf8c66c07cb03b39a4307dda9cb17d670a
Jeehae Park <jpark2973@gmail.com>
2016-03-22 11:23:17 -0700

    (feat/css) Add alert window when users update their info

--- client/users/userController.js
+++ client/users/userController.js
@@ -19,7 +19,7 @@ angular.module('juju.user', [])
          $("#btnSubmit").attr("disabled", false);
          $('#success').html("<div class='alert alert-success'>");
          $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
-         $('#success > .alert-success').append("<strong>Updated! "+ firstName +" </strong>");
+         $('#success > .alert-success').append(" "+ firstName +"!  <strong>Updated! </strong>");
          $('#success > .alert-success').append('</div>');
 
         //clear all fields
