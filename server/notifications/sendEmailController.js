9ceff0dfced65376f7534872ae512fb6f0996c07
James Youn <jamesyoun710@gmail.com>
2016-03-24 14:21:44 -0700

    (email) update email info to 'saja'

--- server/notifications/sendEmailController.js
+++ server/notifications/sendEmailController.js
@@ -20,9 +20,9 @@ module.exports = {
     var message = 'Hello from Saja!\nThis is a notification that the item you were watching (' + itemNickname + ') is currently ' + currentPrice + ' which is equal to or less than the price you were interested in paying. \nYou can find the item at \n' + itemUrl + '\n Enjoy!'; // The body of the text message
 
     var mailOptions = {
-      from: "Admin Juju <juju.mks.juju@gmail.com>", // sender address
+      from: "Saja Notifications <saja.notification@gmail.com>", // sender address
       to: userEmail, // list of receivers
-      subject: "Norification from Saja ", // Subject line
+      subject: "Your item is at your ideal price!", // Subject line
       html: message // html body
     };
     console.log(mailOptions);
